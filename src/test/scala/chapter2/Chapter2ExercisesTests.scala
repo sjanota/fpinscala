e3a3821c55c9271a40d730141f25f606a0407878
Szymon Janota <szymon.janota@sap.com>
2020-04-28 18:55:13 +0200

    Fix tests

--- src/test/scala/chapter2/Chapter2ExercisesTests.scala
+++ src/test/scala/chapter2/Chapter2ExercisesTests.scala
@@ -1,20 +1,18 @@
 package chapter2
 
-import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.flatspec.AnyFlatSpec
 
-class Chapter2ExercisesTests extends AnyFunSpec {
-  describe("fib") {
-    it("should return 0 as first element") {
-      assert(Chapter2Exercises.fib(1) == 0)
-    }
-    it("should return 1 as second element") {
-      assert(Chapter2Exercises.fib(2) == 1)
-    }
-    it("should return 1 as third element") {
-      assert(Chapter2Exercises.fib(3) == 1)
-    }
-    it("should return 21 as 8 element") {
-      assert(Chapter2Exercises.fib(8) == 21)
-    }
+class Chapter2ExercisesTests extends AnyFlatSpec {
+  "fib" should "return 0 as first element" in {
+    assert(Chapter2Exercises.fib(1) == 0)
+  }
+  "fib" should "return 1 as second element" in {
+    assert(Chapter2Exercises.fib(2) == 1)
+  }
+  "fib" should "return 1 as third element" in {
+    assert(Chapter2Exercises.fib(3) == 1)
+  }
+  "fib" should "return 21 as 9th element" in {
+    assert(Chapter2Exercises.fib(9) == 21)
   }
 }
