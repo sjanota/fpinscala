ee9d75c4dddfcf949a12729aef936ca020a41ecd
Szymon Janota <szymon.janota@sap.com>
2020-04-28 19:40:02 +0200

    End chapter 2

--- src/main/scala/chapter2/Chapter2Exercises.scala
+++ src/main/scala/chapter2/Chapter2Exercises.scala
@@ -26,4 +26,13 @@ object Chapter2Exercises {
     if (as.length < 2) true
     else go(1)
   }
+
+  def curry[A, B, C](f: (A, B) => C): A => B => C =
+    (a: A) => (b: B) => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    (a: A) => f(g(a))
 }
--- src/main/scala/chapter2/MyModule.scala
+++ src/main/scala/chapter2/MyModule.scala
@@ -35,4 +35,6 @@ object MyModule {
 
     go(0)
   }
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = (b: B) => f(a, b)
 }
