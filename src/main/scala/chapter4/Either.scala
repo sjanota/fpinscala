59f5ec2bf663d56740e2bd85388d66c23f48d730
Szymon Janota <szymon.janota@sap.com>
2020-05-01 12:27:21 +0200

    Chapter 5 init

--- src/main/scala/chapter4/Either.scala
+++ src/main/scala/chapter4/Either.scala
@@ -1,7 +1,39 @@
 package chapter4
 
-sealed trait Either[+E, +A]
+import scala.{Either => _, Option => _}
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(value) => Right(f(value))
+      case Left(e)      => Left(e)
+    }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(value) => f(value)
+      case Left(e)      => Left(e)
+    }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(value) => Right(value)
+      case Left(e)      => b
+    }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for { a <- this; bb <- b } yield f(a, bb)
+}
 
 case class Left[+E](value: E) extends Either[E, Nothing]
 
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(identity)
+
+  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight[Either[E, List[B]]](Right(Nil))((a, acc) =>
+      f(a).map2(acc)(_ :: _))
+}
