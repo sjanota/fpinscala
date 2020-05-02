f9fd30e324e1434c2d63c88cbac71079f6e5b0a9
Szymon Janota <szymon.janota@sap.com>
2020-05-02 08:13:58 +0200

    6.1 - nonNegativeInt

--- src/main/scala/chapter6/RNG.scala
+++ src/main/scala/chapter6/RNG.scala
@@ -2,17 +2,23 @@ package chapter6
 
 trait RNG {
   def nextInt: (Int, RNG)
-}
 
-case class SimpleRNG(seed: Long) extends RNG {
-  override def nextInt: (Int, RNG) = {
-    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-    val nextRNG = SimpleRNG(newSeed)
-    val n = (newSeed >>> 16).toInt
-    (n, nextRNG)
+  def nonNegativeInt: (Int, RNG) = {
+    val (n, nextRng) = nextInt
+    (if (n >= 0) n else -(n + 1), nextRng)
   }
 }
 
 object RNG {
-  def apply(seed: Long): RNG = SimpleRNG(seed)
+  def apply(seed: Long): RNG = Simple(seed)
+
+  case class Simple(seed: Long) extends RNG {
+    override def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
 }
