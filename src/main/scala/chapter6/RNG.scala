96e31a48ace921115e9bc5889b316765d3da0771
Szymon Janota <szymon.janota@sap.com>
2020-05-02 09:05:30 +0200

    6.{3,4} - more tedious generators + state attempt

--- src/main/scala/chapter6/RNG.scala
+++ src/main/scala/chapter6/RNG.scala
@@ -3,16 +3,45 @@ package chapter6
 trait RNG {
   def nextInt: (Int, RNG)
 
-  def nonNegativeInt: (Int, RNG) = {
-    val (n, nextRng) = nextInt
-    (if (n >= 0) n else -(n + 1), nextRng)
-  }
+  def nonNegativeInt: (Int, RNG) =
+    nonNegativeIntS.toTuple
 
-  def double: (Double, RNG) = {
-    val (n, nextRng) = nonNegativeInt
-    (n / (Int.MaxValue.toDouble + 1), nextRng)
-  }
+  def intDouble: ((Int, Double), RNG) =
+    intDoubleS.toTuple
 
+  private def intDoubleS =
+    State(nextInt) flatMap ((i, s) => State(s.double) map ((i, _)))
+
+  def double: (Double, RNG) =
+    doubleS.toTuple
+
+  private def doubleS: State[Double, RNG] =
+    nonNegativeIntS map (_ / (Int.MaxValue.toDouble + 1))
+
+  def nonNegativeIntS: State[Int, RNG] =
+    State(nextInt).map(n => if (n >= 0) n else -(n + 1))
+
+  def doubleInt: ((Double, Int), RNG) =
+    intDoubleS.map(t => (t._2, t._1)).toTuple
+
+  def double3: ((Double, Double, Double), RNG) =
+    doubleS
+      .flatMap((d1, s1) =>
+        s1.doubleS.flatMap((d2, s2) => s2.doubleS.map((d1, d2, _))))
+      .toTuple
+
+  def ints(count: Int): (List[Int], RNG) = {
+    @scala.annotation.tailrec
+    def go(n: Int, rng: RNG, acc: List[Int]): (List[Int], RNG) = {
+      if (n == 0) (acc, rng)
+      else {
+        val (i, rng0) = rng.nextInt
+        go(n - 1, rng0, i :: acc)
+      }
+    }
+
+    go(count, this, Nil)
+  }
 }
 
 object RNG {
@@ -27,4 +56,23 @@ object RNG {
     }
   }
 
+}
+
+case class State[A, S](a: A, s: S) {
+
+  def map[B](f: A => B): State[B, S] =
+    State(f(a), s)
+
+  def mapState[S1](f: S => S1): State[A, S1] =
+    State(a, f(s))
+
+  def flatMap[B](f: (A, S) => State[B, S]): State[B, S] =
+    f(a, s)
+
+  def toTuple: (A, S) =
+    (a, s)
+}
+
+object State {
+  def apply[A, S](t: (A, S)): State[A, S] = State(t._1, t._2)
 }
