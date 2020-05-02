b7f51f0425842ef1531d30d44b1b51e371e0133d
Szymon Janota <szymon.janota@sap.com>
2020-05-02 08:06:58 +0200

    6 - init

---
+++ src/main/scala/chapter6/RNG.scala
@@ -0,0 +1,18 @@
+package chapter6
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  override def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+object RNG {
+  def apply(seed: Long): RNG = SimpleRNG(seed)
+}
