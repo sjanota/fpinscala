5562cc5479015dc84d7e21ad8b98da1102730104
Szymon Janota <szymon.janota@sap.com>
2020-05-15 17:23:25 +0200

    Additional generators

--- src/main/scala/chapter8/Gen.scala
+++ src/main/scala/chapter8/Gen.scala
@@ -1,9 +1,53 @@
 package chapter8
 
-trait Gen[T]
+import chapter6.{RNG, State}
+import chapter8.Gen._
+
+case class Gen[A](sample: State[RNG, A]) {
+  def map[B](f: A => B): Gen[B] =
+    map2(unit(()))((x, _) => f(x))
+
+  def map2[B, C](other: Gen[B])(f: (A, B) => C): Gen[C] =
+    Gen(for {
+      aa <- sample
+      bb <- other.sample
+    } yield f(aa, bb))
+
+}
 
 object Gen {
   def listOf[A](g: Gen[A]): Gen[List[A]] = ???
 
-  def choose[A](min: A, max: A): Gen[A] = ???
+  def alphaNumericN(n: Int): Gen[String] =
+    listOfN(n, alphaNumericChar) map (_.mkString)
+
+  def alphaNumericChar: Gen[Char] =
+    oneOf(choose(65, 90), choose(97, 122)) map (_.toChar)
+
+  def oneOf[A](a1: Gen[A], a2: Gen[A]): Gen[A] =
+    option(a1).map2(a2)((oa1, a2) => oa1 getOrElse a2)
+
+  def option[A](a: Gen[A]): Gen[Option[A]] =
+    boolean.map2(a)((isEmpty, aa) => if (isEmpty) None else Some(aa))
+
+  def boolean: Gen[Boolean] =
+    Gen(RNG.int map (_ % 2 == 0))
+
+  def unit[A](a: => A): Gen[A] =
+    Gen(State.unit(a))
+
+  def tuple2[A, B](a: Gen[A], b: Gen[B]): Gen[(A, B)] =
+    a.map2(b)((_, _))
+
+  def stringN(n: Int): Gen[String] =
+    listOfN(n, char) map (_.mkString)
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.traverse(List.range(0, n))(_ => g.sample))
+
+  def char: Gen[Char] =
+    choose(0, 255) map (_.toChar)
+
+  def choose(min: Int, max: Int): Gen[Int] =
+    Gen(RNG.double map (d => Math.round(d * (max - min).toDouble + min).toInt))
 }
