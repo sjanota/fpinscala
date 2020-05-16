9d1b27d2aff47b68a88b39e66e9367a06972f160
Szymon Janota <szymon.janota@sap.com>
2020-05-16 14:37:39 +0200

    For All

--- src/main/scala/chapter8/Prop.scala
+++ src/main/scala/chapter8/Prop.scala
@@ -1,14 +1,28 @@
 package chapter8
 
+import chapter6.RNG
 import chapter8.Prop._
 
-trait Prop {
-  def check: Either[(FailedCase, SuccessCount), SuccessCount]
-
+case class Prop(run: (TestCases, RNG) => Result) {
   def &&(other: Prop): Prop = ???
 }
 
 object Prop {
+  type TestCases = Int
   type FailedCase = String
   type SuccessCount = Int
+
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+
+  case class Falsified(failure: FailedCase, successes: SuccessCount)
+      extends Result {
+    override def isFalsified: Boolean = true
+  }
+
+  case object Passed extends Result {
+    override def isFalsified: Boolean = false
+  }
+
 }
--- src/main/scala/chapter8/package.scala
+++ src/main/scala/chapter8/package.scala
@@ -1,3 +1,25 @@
+import chapter5.Stream
+import chapter6.RNG
+import chapter8.Prop.{Falsified, Passed}
+
 package object chapter8 {
-  def forAll[A](g: Gen[A])(p: A => Boolean): Prop = ???
+  def forAll[A](g: Gen[A])(p: A => Boolean): Prop = Prop { (n, rng) =>
+    randomStream(g, rng) zip Stream.from(0) take n map {
+      case (a, i) =>
+        try {
+          if (p(a)) Passed
+          else Falsified(a.toString, i)
+        } catch {
+          case e: Exception => Falsified(buildMsg(a, e), i)
+        }
+    } find (_.isFalsified) getOrElse Passed
+  }
+
+  def buildMsg[A](a: A, e: Exception): String =
+    s"test case: $a\n" +
+      s"generated an exception: $e\n" +
+      s"stack trace:\n${e.getStackTrace.mkString("\n")}"
+
+  def randomStream[A](g: Gen[A], rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample(rng)))
 }
